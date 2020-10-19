2e90021336051ddf37a5fff41a3f1159e9f8d924
davidWei <david120911@gmail.com>
2020-10-19 09:43:49 +0800

    delete book package

--- src/book/v2ch09/ch09.sec03/com/horstmann/hello/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package book.v2ch09.ch09.sec03.com.horstmann.hello;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-       System.out.println("Hello, Modular World!");
-    }
-}
--- src/book/v2ch09/ch15.sec04/com/horstmann/hello/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package book.v2ch09.ch15.sec04.com.horstmann.hello;
-
-import javax.swing.JOptionPane;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello, Modular World!");
-    }
-}
