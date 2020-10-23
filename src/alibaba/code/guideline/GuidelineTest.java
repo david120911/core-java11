4bc5c22dcd5057a918cab296186d232523dc30d5
davidWei <david120911@gmail.com>
2020-10-24 00:42:56 +0800

    alibaba java code guideline

---
+++ src/alibaba/code/guideline/GuidelineTest.java
@@ -0,0 +1,13 @@
+package alibaba.code.guideline;
+
+
+
+public class GuidelineTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is for alibaba java code guideline practice ");
+
+    }
+
+}
