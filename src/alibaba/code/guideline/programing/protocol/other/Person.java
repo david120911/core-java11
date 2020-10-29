349d9f9fe8f20ff874dc040c029a49c0b57b198b
davidWei <david120911@gmail.com>
2020-10-29 14:50:22 +0800

    other code commit

---
+++ src/alibaba/code/guideline/programing/protocol/other/Person.java
@@ -0,0 +1,23 @@
+package alibaba.code.guideline.programing.protocol.other;
+
+public class Person {
+    private boolean healthy;
+
+    private boolean wealthy;
+
+    public boolean isWealthy() {
+        return wealthy;
+    }
+
+    public void setWealthy(boolean wealthy) {
+        this.wealthy = wealthy;
+    }
+
+    public boolean isHealthy() {
+        return healthy;
+    }
+
+    public void setHealthy(boolean healthy) {
+        this.healthy = healthy;
+    }
+}
